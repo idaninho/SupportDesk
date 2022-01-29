04cd508d1a7e0f4f17b7556ac5c37a475965d9f8
Idan <idaninho@gmail.com>
2022-01-29 13:18:56 +0200

    finished authMiddleware with jwt, starting frontend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const morgan = require('morgan');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const PORT = process.env.PORT || 8000;
@@ -9,6 +10,10 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.get('/', (req, res) => {
   res.send('Hello');
